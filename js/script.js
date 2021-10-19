51f8e9425cb21131204fd0b06313691ae5c022ae
Shubham Pant <shubhampant.mic@gmail.com>
2021-10-19 21:51:14 +0530

    final changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //gets the carousel class div
 const carousel = document.querySelector('.carousel__images')
 //takes button class on click event
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.btn')
 //grab all the images(img) with className .carousel__images
 const totalImages = document.querySelectorAll('.carousel__images img').length
 
@@ -19,21 +19,20 @@ buttons.forEach(button =>{
             if(imageIndex > 0){
                 imageIndex--
                 translateX += 300
-
             }
         }else{
             //if the button id is "next" then it shifts the image position to 300px left
             if(imageIndex < totalImages){
                 imageIndex++
                 translateX = translateX - 300
-                console.log("next")
+            }
+            if(imageIndex === totalImages){
+                imageIndex = 0
+                translateX = 0
             }
         }
         //if the imageIndex is at the last image then it makes position to 0 to shift it to first image
-        if(imageIndex === totalImages){
-            imageIndex = 0
-            translateX = 0
-        }
+        
         carousel.style.transform = `translateX(${translateX}px)`
     })
 })
